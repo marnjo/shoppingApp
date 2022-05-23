ee41012478629d0e5ace46de729815d4e709e54e
Ikubi <ikubij@gmail.com>
2022-05-23 10:58:45 +0300

    fix, enable mutate state to increase amount

--- src/app/cart/store/cart.reducer.ts
+++ src/app/cart/store/cart.reducer.ts
@@ -2,57 +2,86 @@ import { IProduct } from '../../shared/interfaces';
 import * as CartActions from './cart.actions';
 
 export interface State {
-    cart: IProduct[];
+  cart: IProduct[];
 }
 
 const initialState = {
-    cart: [],
-}
+  cart: [],
+};
 
-export function CartReducer(state = initialState, action: CartActions.CartActions) {
-    const newCart = state.cart.slice();
-    switch(action.type) {
-        case CartActions.ADD_TO_CART:
-            // if (newCart.find(elem => elem.id === action.payload.product.id)) {
-            //     const index = newCart.map(elem => elem['id']).indexOf(action.payload.product.id);
-            //     console.log();
-            //     console.log(true);
-            //     console.log(newCart[index]['amount']);
-            //     newCart[index]['amount']++;
-            //     console.log(newCart.map(elem => elem['id']).indexOf(action.payload.product.id));
-            // } else {
-                newCart.push(action.payload.product);
-                console.log();
-                console.log(false);
-                console.log(newCart.find(elem => elem.id === action.payload.product.id));
-            // }
-            return {
-                ...state,
-                cart: newCart
-            }
-        case CartActions.REMOVE_FROM_CART:
-            // const newCart = state.cart.slice();
-            // if (newCart.find(elem => elem.id === action.payload.product.id)) {
-            //     const index = newCart.map(elem => elem['id']).indexOf(action.payload.product.id);
-            //     console.log();
-            //     console.log(true);
-            //     console.log(newCart[index]['amount']);
-            //     newCart[index]['amount']++;
-            //     console.log(newCart.map(elem => elem['id']).indexOf(action.payload.product.id));
-            // } else {
-                const index = newCart.map(elem => elem['id']).indexOf(action.payload.product.id);
-                newCart.splice(index, 1);
-                console.log();
-                console.log(false);
-                console.log(newCart.find(elem => elem.id === action.payload.product.id));
-            // }
-            return {
-                ...state,
-                cart: newCart
-            }
-        default:
-            return {
-                ...state
-            }
-    }
+export function CartReducer(
+  state = initialState,
+  action: CartActions.CartActions
+) {
+  let newCart = [...state.cart.slice()];
+  switch (action.type) {
+    case CartActions.ADD_TO_CART:
+      //   if (newCart.find(elem => elem.id === action.payload.product.id)) {
+      //       const index = newCart.map(elem => elem['id']).indexOf(action.payload.product.id);
+      //       console.log();
+      //       console.log(true);
+      //       console.log(newCart[index]['amount']);
+      //       newCart[index]['amount']++;
+      //       console.log(newCart.map(elem => elem['id']).indexOf(action.payload.product.id));
+      //   } else {
+      //   newCart.push(action.payload.product);
+      //   console.log();
+      //   console.log(false);
+      //   console.log(newCart.find(elem => elem.id === action.payload.product.id));
+      //   }
+      const itemExistsIndex = newCart.findIndex(
+        (elem) => elem.id === action.payload.product.id
+      );
+
+      if (itemExistsIndex > -1) {
+        let existingItem = Object.assign({}, newCart[itemExistsIndex]);
+
+        let newObj = {
+          ...existingItem,
+          amount: +existingItem.amount + 1,
+        };
+
+        newCart[itemExistsIndex] = newObj;
+      } else {
+        newCart.push({
+          ...action.payload.product,
+          amount: 1,
+        });
+      }
+
+      console.log(newCart);
+
+      return {
+        ...state,
+        cart: newCart,
+      };
+    case CartActions.REMOVE_FROM_CART:
+      // const newCart = state.cart.slice();
+      // if (newCart.find(elem => elem.id === action.payload.product.id)) {
+      //     const index = newCart.map(elem => elem['id']).indexOf(action.payload.product.id);
+      //     console.log();
+      //     console.log(true);
+      //     console.log(newCart[index]['amount']);
+      //     newCart[index]['amount']++;
+      //     console.log(newCart.map(elem => elem['id']).indexOf(action.payload.product.id));
+      // } else {
+      const index = newCart
+        .map((elem) => elem['id'])
+        .indexOf(action.payload.product.id);
+      newCart.splice(index, 1);
+      console.log();
+      console.log(false);
+      console.log(
+        newCart.find((elem) => elem.id === action.payload.product.id)
+      );
+      // }
+      return {
+        ...state,
+        cart: newCart,
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
 }
--- src/app/products/product-item/product-item.component.ts
+++ src/app/products/product-item/product-item.component.ts
@@ -8,34 +8,35 @@ import * as cartActions from '../../cart/store/cart.actions';
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.scss']
+  styleUrls: ['./product-item.component.scss'],
 })
 export class ProductItemComponent implements OnInit {
   @Input() product: IProduct;
   count: number;
 
-  constructor(
-    private store: Store<fromApp.AppState>
-  ) { }
+  constructor(private store: Store<fromApp.AppState>) {}
 
   ngOnInit(): void {
     this.count = this.product.amount;
-    this.store.select('cart').subscribe(
-      (cartState) => {
-        if (this.product.id == 1) console.log(cartState)
-        this.count = cartState.cart.filter(prod => prod['id'] == this.product['id']).length;
+    this.store.select('cart').subscribe((cartState) => {
+      if (this.product.id == 1) {
+        // console.log(cartState)
       }
-    );
+      this.count = cartState.cart.filter(
+        (prod) => prod['id'] == this.product['id']
+      ).length;
+    });
     // console.log(this.product);
   }
 
   subtractCount(): void {
     if (this.count <= 0) return;
-    this.store.dispatch(new cartActions.RemoveFromCart({product: this.product}));
-  }
-  
-  addCount(): void {
-    this.store.dispatch(new cartActions.AddToCart({product: this.product}));
+    this.store.dispatch(
+      new cartActions.RemoveFromCart({ product: this.product })
+    );
   }
 
+  addCount(): void {
+    this.store.dispatch(new cartActions.AddToCart({ product: this.product }));
+  }
 }
